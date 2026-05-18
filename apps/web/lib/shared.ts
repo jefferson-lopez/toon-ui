d3c2a901b997d1de9e900014bf2a94c558d9b54d
Jefferson Lopez <jeffersonlopezmendoza343@gmail.com>
2026-05-18 15:48:15 -0700

    feat(docs): add fumadocs documentation app

---
+++ apps/web/lib/shared.ts
@@ -0,0 +1,4 @@
+export const appName = 'ToonUI';
+export const docsTitle = 'ToonUI Documentation';
+export const repoUrl = 'https://github.com/jefferson-lopez/toon-ui';
+export const repoBlobUrl = `${repoUrl}/blob/main`;
